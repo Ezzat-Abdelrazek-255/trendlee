3e9bda3e4de69d01a472c7b54e31f996ac480ea0
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-12-19 22:32:14 +0200

    feat: nav disappear on scroll and appear on scroll end

--- components/process-section/process-card.tsx
+++ components/process-section/process-card.tsx
@@ -16,11 +16,11 @@ const ProcessCard = ({ title, imgSrc, index }: ProcessCardType) => {
       className={cn(
         "mt-[10rem] grid h-[50rem] w-full max-w-[60rem] grid-cols-[auto_1fr] gap-[4.8rem] md:translate-x-[2.5rem] md:grid-cols-[1fr_auto] md:justify-self-end",
         isEven &&
-        "md:mt-[60rem] md:-translate-x-[2.5rem] md:grid-cols-[auto_1fr] md:justify-self-start",
+          "md:mt-[60rem] md:-translate-x-[2.5rem] md:grid-cols-[auto_1fr] md:justify-self-start",
       )}
     >
       <ProcessTimelineMilestone index={index} />
-      <div>
+      <div className="relative">
         <h3
           className={cn(
             "text-right font-display text-[6rem] uppercase leading-[85%]",
@@ -29,16 +29,19 @@ const ProcessCard = ({ title, imgSrc, index }: ProcessCardType) => {
         >
           {title}
         </h3>
-        <div className="relative z-0 h-[30rem] rounded-[1rem] sm:h-[40rem]">
-          <div className="absolute inset-0 z-base bg-black/20"></div>
-          <div className="absolute inset-0 z-base bg-primary/5"></div>
-          <Image
-            src={imgSrc}
-            alt={title}
-            className="rounded-inherit h-full w-full object-cover"
-            fill
-            sizes="50vw"
-          />
+        <div className="-translate-y-[6rem]">
+          <div className="process-mask absolute inset-0 z-hidden h-[30rem] -translate-x-[1rem] translate-y-[1rem] rounded-[1rem] bg-primary/25 sm:h-[40rem]"></div>
+          <div className="process-mask relative z-0 h-[30rem] rounded-[1rem] sm:h-[40rem]">
+            <div className="absolute inset-0 z-base bg-black/20"></div>
+            <div className="absolute inset-0 z-base bg-primary/5"></div>
+            <Image
+              src={imgSrc}
+              alt={title}
+              className="rounded-inherit h-full w-full object-cover"
+              fill
+              sizes="50vw"
+            />
+          </div>
         </div>
       </div>
     </article>
--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -1,8 +1,36 @@
+"use client";
+
 import { navItems } from "@/constants";
+import { useGSAP } from "@gsap/react";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Navigation = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useGSAP(
+    function () {
+      const ease = "power1.out";
+      ScrollTrigger.addEventListener("scrollStart", function () {
+        gsap.to("nav", {
+          opacity: 0,
+          y: "-100%",
+          ease,
+        });
+      });
+      ScrollTrigger.addEventListener("scrollEnd", function () {
+        gsap.to("nav", {
+          opacity: 1,
+          y: "0",
+          ease,
+        });
+      });
+    },
+    { scope: containerRef },
+  );
   return (
     <nav className="hidden md:block">
       <ul className="relative flex items-center justify-between gap-[3.2rem] rounded-full border-2 border-primary bg-primary/50 p-[3.2rem] text-[1.6rem] uppercase leading-[85%] backdrop-blur-sm after:absolute after:inset-0 after:z-hidden after:bg-primary/25 after:blur-[2.4rem] after:content-['']">
