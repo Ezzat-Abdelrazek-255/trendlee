40e58e4484b3d97fbdf63e64b6a0a2397a5521db
Ezzat Abdelrazek <ezzatabdelrazek255@gmail.com>
2024-12-31 02:24:51 +0200

    commit

--- constants/offers.ts
+++ constants/offers.ts
@@ -286,7 +286,7 @@ export const paymentSuccessEmailMarkup = `
         .btn {
             display: inline-block;
             padding: 10px 20px;
-            background-color: #28a745;
+            background-color: #007BFF;
             color: #fff;
             text-decoration: none;
             border-radius: 5px;
